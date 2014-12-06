21a2d34e0c2b2299d188f2971baacb62485cae97
r3vl1s <timothy.b.seifert@gmail.com>
2014-12-06 15:51:28 -0600

    reskin

--- build/clumsy.js
+++ build/clumsy.js
@@ -41,9 +41,9 @@ var game = {
 game.resources = [
     // images
     {name: "bg", type:"image", src: "data/img/bg.png"},
-    {name: "clumsy", type:"image", src: "data/img/clumsy.png"},
-    {name: "pipe", type:"image", src: "data/img/pipe.png"},
-    {name: "logo", type:"image", src: "data/img/logo.png"},
+    {name: "clumsy", type:"image", src: "data/img/shane.png"},
+    {name: "pipe", type:"image", src: "data/img/sh_pipe.png"},
+    {name: "logo", type:"image", src: "data/img/logo_barkley.png"},
     {name: "ground", type:"image", src: "data/img/ground.png"},
     {name: "gameover", type:"image", src: "data/img/gameover.png"},
     {name: "gameoverbg", type:"image", src: "data/img/gameoverbg.png"},
